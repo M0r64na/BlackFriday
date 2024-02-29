d4186b03e6cdccb59897d7b427abff39f5df3d53
M0r64na <kiara.lazarova@yahoo.com>
2024-02-29 17:47:09 +0200

    :recycle: Extracted local vars from methods as class static final fields

--- BlackFriday/src/main/java/common/ConsoleCallbackHandler.java
+++ BlackFriday/src/main/java/common/ConsoleCallbackHandler.java
@@ -2,11 +2,11 @@ package common;
 
 import javax.security.auth.callback.*;
 import java.io.BufferedReader;
-import java.io.Console;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class ConsoleCallbackHandler implements CallbackHandler {
+    private static final BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
     @Override
     public void handle(Callback[] callbacks) throws IOException, UnsupportedCallbackException {
          for(Callback c : callbacks) {
@@ -26,13 +26,13 @@ public class ConsoleCallbackHandler implements CallbackHandler {
 
     private void handleNameCallback(NameCallback callback) throws IOException {
         System.out.print(callback.getPrompt());
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
         callback.setName(reader.readLine());
     }
 
     private void handlePasswordCallback(PasswordCallback callback) throws IOException {
         System.out.print(callback.getPrompt());
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
         char[] password = reader.readLine().toCharArray();
         callback.setPassword(password);
     }
